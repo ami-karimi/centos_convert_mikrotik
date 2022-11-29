51b5e8de54d2e9e19c1dd39e66430113e902d755
amir karimi <takfashomal@gmail.com>
2022-11-29 14:51:04 +0330

    first

--- convert_sda.sh
+++ convert_sda.sh
@@ -7,7 +7,7 @@ ADDRESS=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=$GATEWAY
-/user set 0 name=admin password=admin
+/user set 0 name=admin password=Amir@##1401
  " > /mnt/rw/autorun.scr && \
 umount /mnt && \
 echo u > /proc/sysrq-trigger && \

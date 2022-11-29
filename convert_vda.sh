a7ba1f59b2a34d7f16a2536d4b3ad6cbff2a3920
amir karimi <takfashomal@gmail.com>
2022-11-29 14:00:00 +0330

    first

---
+++ convert_vda.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+wget https://download.mikrotik.com/routeros/7.5/chr-7.5.img.zip -O chr.img.zip  && \
+gunzip -c chr.img.zip > chr.img  && \
+mount -o loop,offset=512 chr.img /mnt && \
+ADDRESS=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
+GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
+echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
+/ip route add gateway=$GATEWAY
+/user set 0 name=admin password=admin
+ " > /mnt/rw/autorun.scr && \
+umount /mnt && \
+echo u > /proc/sysrq-trigger && \
+dd if=chr.img bs=38M of=/dev/vda && \
+echo "sync disk" && \
+echo s > /proc/sysrq-trigger && \
+echo "Sleep 5 seconds" && \
+sleep 5 && \
+echo "Ok, reboot" && \
+echo b > /proc/sysrq-trigger
